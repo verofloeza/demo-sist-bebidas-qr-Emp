75a0d8fa68e5e8ce8ac651f8513f28cc5c3dc558
verofloeza <verofloeza@gmail.com>
2024-01-24 17:19:11 -0300

    sub

--- src/component/componentProducts/ModalQr.js
+++ src/component/componentProducts/ModalQr.js
@@ -23,7 +23,7 @@ const ModalQr = ({modal, toggle}) => {
     const urlModificada = urlConDominio ? result : cambiarDominio(result, "demo-sist-bebidas-qr-emp.vercel.app");
 
     // Navegar a la nueva URL
-    history(urlModificada);
+    history(urlModificada.substring(30));
   };
 
   // Función que se ejecuta en caso de error en el escaneo
