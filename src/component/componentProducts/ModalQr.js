719b84dc12999923a8b181e49d7a9666b176ee73
verofloeza <verofloeza@gmail.com>
2024-01-24 17:37:18 -0300

    local

--- src/component/componentProducts/ModalQr.js
+++ src/component/componentProducts/ModalQr.js
@@ -8,23 +8,13 @@ const ModalQr = ({modal, toggle}) => {
     const history = useNavigate();
     const [ result, setResult ] = useState(null)
 
-    // Función para cambiar el dominio de la URL
-  const cambiarDominio = (urlOriginal, nuevoDominio) => {
-    // Reemplazar el dominio actual con el nuevo dominio
-    return urlOriginal.replace(/^https:\/\/[^\/]+/, `https://${nuevoDominio}`);
-  };
-
   // Función que se ejecuta cada vez que se decodifica un nuevo QR
   const handleDecode = (result) => {
-    alert(result)
-    // // Extraer la parte de la URL después de 'https://demo-sist-bebidas-qr-emp.vercel.app/ordenes/'
-    // const parteDespuesDeOrdenes = result.substring('https://demo-sist-bebidas-qr-emp.vercel.app/ordenes/'.length);
+    // Extraer la parte de la URL después de 'https://demo-sist-bebidas-qr-emp.vercel.app/ordenes/'
+    const parteDespuesDeOrdenes = result.substring('https://localhost:3001/'.length);
 
-    // // Construir la nueva URL
-    // const nuevaUrl = `/ordenes/${parteDespuesDeOrdenes}`;
-
-    // // Redireccionar a la nueva URL
-    // navigate(nuevaUrl);
+    // Redireccionar a la nueva URL
+    navigate(parteDespuesDeOrdenes);
   };
 
   // Función que se ejecuta en caso de error en el escaneo
